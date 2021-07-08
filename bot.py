063b11897ceb33145a3113f916634c9b56cbdc4c
Nikita Sergievsky <reetray@protonmail.com>
2021-07-08 14:53:17 +0300

    Добавил возможность делать бекапы гифок из Телеграма в ВК

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 	VKGIFSBot Inline Telegram bot that receives GIF images from VK
 	yepIwt, 2021
 """
+
 import os
 import hashlib
 from aiogram import Bot, Dispatcher, executor, types
@@ -11,11 +12,15 @@ from vkwave.client import AIOHTTPClient
 import db
 from urllib.parse import parse_qs
 
+import aiohttp
+import moviepy.editor as mp
+
 GREATING = "Привет, этот бот поможет тебе отправлять GIF-изображения из ВКонтакте в Телеграме, войди по кнопке ниже и отправь мне то, что получишь в адресной строке."
 AUTH_URL = "https://oauth.vk.com/authorize?client_id=7894722&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=docs,offline&response_type=token&v=5.52"
 PARANOID_URL = "https://github.com/yepIwt/VKGIFSBot#vk-gifs-bot"
 
-TOKEN = os.getenv("TELEGRAM_API_TOKEN")
+#TOKEN = os.getenv("TELEGRAM_API_TOKEN")
+TOKEN = "1707022774:AAHA329lEctBUWnQ640YgHfrIiu8jQJjaNI"
 
 class VKGIFSBot(object):
 
@@ -26,17 +31,7 @@ class VKGIFSBot(object):
 
 		self.bot = Bot(token = tg_token)
 
-	def is_it_gif(self, filename: str):
-		try:
-			ext = title[-3:]
-		except Exception:
-			return False
-		else:
-			if ext == 'gif':
-				return True
-			return False
-
-	def create_query_item(result_id: int, gif_url: str):
+	def create_query_item(self, result_id: str, gif_url: str):
 		new_item = types.InlineQueryResultGif(
 			id=result_id,
 			gif_url = gif_url,
@@ -47,14 +42,13 @@ class VKGIFSBot(object):
 		return new_item
 
 	async def get_gifs_from_vk(self, telegram_user_id: int):
-		vk_api_result = await self.APIS.get(str(telegram_user_id)).docs.get()
+		vk_api_result = await self.APIS.get(str(telegram_user_id)).docs.get(type = 3)
 		for file in vk_api_result.response.items:
-			if self.is_it_gif(file.title):
-				item = create_item(
-					result_id = hashlib.md5(str(file.id).encode()).hexdigest(),
-					gif_url = file.url,
-				)
-				self.ALL_GIFS[str(telegram_user_id)].append(item)
+			item = self.create_query_item(
+				result_id = hashlib.md5(str(file.id).encode()).hexdigest(),
+				gif_url = file.url,
+			)
+			self.ALL_GIFS[str(telegram_user_id)].append(item)
 
 	def get_vk_api(self, vk_token: str):
 		api_session = API(tokens=vk_token, clients=AIOHTTPClient())
@@ -107,6 +101,53 @@ class VKGIFSBot(object):
 			db.add(f"{user_id}", vk_token[0])
 			self.APIS.setdefault(f"{user_id}", api)
 			self.OFFSETS.setdefault(f"{user_id}", 1)
+
+	async def backup_info(self, msg: types.Message):
+		user_id = msg.from_user.id
+
+		self.APIS.setdefault(f"{user_id}", None)
+
+		if not self.APIS[str(user_id)]:
+			if db.get_vk_token_by_telegram_id(user_id):
+				await msg.answer("Для бэкапа гифки в вк просто отправьте ее боту")
+			else:
+				await msg.answer("Для начала авторизуйтесь!")
+
+	async def backup_gif(self, msg: types.Message):
+		user_id = msg.from_user.id
+
+		if not self.APIS[str(user_id)]:
+			has_token = db.get_vk_token_by_telegram_id(user_id)
+			if has_token:
+				self.APIS[str(user_id)] = self.get_vk_api(has_token)
+			else:
+				await msg.answer("Сначала авторизуйтесь")
+				return
+
+		await self.bot.download_file_by_id(msg.animation.file_id,'animation.mp4')
+		clip = mp.VideoFileClip('animation.mp4')
+		clip.write_gif('animation.gif',logger=None)
+		vk_api_answer = await self.APIS[str(msg.from_user.id)].docs.get_upload_server()
+		url_for_upload = vk_api_answer.response.upload_url
+		f = open('animation.gif','rb')
+
+		async with aiohttp.ClientSession() as session:
+			async with session.post(url_for_upload, data = {'file':f}) as resp:
+				json = await resp.json()
+				file_obj = json['file']
+
+		vk_api_answer = await self.APIS[str(msg.from_user.id)].docs.save(
+				file = file_obj,
+				title = f'{msg.from_user.first_name}_{msg.date}.gif',
+				tags = '',
+				return_tags = 0
+		)
+
+		f.close()
+		os.remove('animation.gif')
+		os.remove('animation.mp4')
+
+		await msg.answer('Забэкапленно!')
 
 	async def send_welcome(self, message: types.Message):
 		user_id = message.from_user.id
@@ -163,7 +204,9 @@ class VKGIFSBot(object):
 	def start(self):
 		dp = Dispatcher(self.bot)
 		dp.register_message_handler(self.send_welcome, commands = ['start'])
-		dp.register_message_handler(self.await_vk_token, content_types = ['any'])
+		dp.register_message_handler(self.backup_info, commands = ['backup'])
+		dp.register_message_handler(self.await_vk_token, content_types = ['text'])
+		dp.register_message_handler(self.backup_gif, content_types = ['animation']) # странно, что animation - это гиф
 		dp.register_inline_handler(self.inline_process)
 		executor.start_polling(dp, skip_updates = True)
 
